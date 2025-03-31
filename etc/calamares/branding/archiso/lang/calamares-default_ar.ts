76bc4dcc922e3b3068bc8dbce450e9efecb8324d
Itsuki Wada <175463719+Itsuki0222@users.noreply.github.com>
2025-03-31 09:52:48 +0900

    Update calamares-default_ar.ts

--- etc/calamares/branding/archiso/lang/calamares-default_ar.ts
+++ etc/calamares/branding/archiso/lang/calamares-default_ar.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ar">
+<TS version="2.1" language="ja">
 <context>
     <name>show</name>
     <message>
         <location filename="../show.qml" line="64"/>
         <source>This is a second Slide element.</source>
-        <translation>عرض الثاني</translation>
+        <translation>これは第2のスライド要素である。</translation>
     </message>
     <message>
         <location filename="../show.qml" line="68"/>
         <source>This is a third Slide element.</source>
-        <translation>عرض الثالث</translation>
+        <translation>これは第3のスライド要素である</translation>
     </message>
 </context>
 </TS>
